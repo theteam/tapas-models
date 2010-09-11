56811fdccf90cff93f793d19c57b9f742787260a
Robbie Clutton <robert.clutton@gmail.com>
2010-09-11 13:03:38 +0100

    model init

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var model = model || {};
+
+model.user = require('src/user');
+model.news = require('src/news');
+
+module.exports = model;
---
+++ src/user.js
@@ -0,0 +1,42 @@
+var mongoose = require('../modules/mongoose/mongoose').Mongoose;
+var db = mongoose.connect('mongodb://localhost/tapas');
+
+mongoose.model('User', {
+
+    properties: ['first', 'last', 'age', 'updated_at'],
+
+    cast: {
+      age: Number,
+      'nested.path': String
+    },
+
+    indexes: ['first'],
+
+    setters: {
+        first: function(v){
+            return v.toLowerCase();
+        }
+    },
+
+    getters: {
+        full_name: function(){ 
+            return this.first + ' ' + this.last; 
+        }
+    },
+
+    methods: {
+        save: function(fn){
+            this.updated_at = new Date();
+            this.__super__(fn);
+        }
+    },
+
+    static: {
+        findOldPeople: function(){
+            return this.find({age: { '$gt': 70 }});
+        }
+    }
+
+});
+
+module.exports = db.model('User');
