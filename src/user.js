f4accf2b0342583facda9130a6f087a0af6134f7
Robbie Clutton <robert.clutton@gmail.com>
2010-09-21 12:03:50 +0100

    check if array

--- src/user.js
+++ src/user.js
@@ -8,6 +8,16 @@ mongoose.model('User', {
 
     'indexes': ['first', 'last', [{'username':1},{unique: true}]],
 
+	'setters': {
+		clients : function(c) {
+			if (!(c instanceof Array)){
+				var result = new Array();
+				result[0] = c;
+				return result;
+			}
+		}
+	},
+
     'getters': {
         full_name: function(){ 
             return this.first + ' ' + this.last; 
